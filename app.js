42b738d1b486cafd75f2e35c918d42586c4e8633
Nick-roy <nroy08232@gmail.com>
2025-02-08 21:52:40 +0600

    2nd commit

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-const add = require("./math.js"); // Add the .js extension
-console.log(add(5, 10));
+// const add = require("./math.js"); // Add the .js extension
+// const mul = require("./math.js"); // Add the .js extension
+const math = require("./math.js"); 
+console.log(math.add(5 , 10));
+console.log(math.mul(5 , 10));
+console.log(math.sub(5 , 10));
+console.log(math.mul(5 , 10));
--- math.js
+++ math.js
@@ -1,4 +1,13 @@
 const add = (a, b) => {
     return a + b;
 }
-module.exports = add;
+const sub = (a, b) => {
+    return a - b;
+}
+const mul = (a, b) => {
+    return a * b;
+}
+const div = (a, b) => {
+    return a / b;
+}
+module.exports ={add, sub,mul,div}
